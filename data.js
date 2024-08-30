d377d950c944e3cfbe3a5ea82a206e3934ff2655
Nithin <nithinv365@gmail.com>
2024-08-30 17:20:08 +0530

    deploy

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'music/song1.m4a',
+        artist: 'artist 1',
+        cover: 'images/3794456.webp'
+    },
+    {
+        name: 'song 2',
+        path: 'music/song2.mp3',
+        artist: 'artist 2',
+        cover: 'images/410996.webp'
+    },
+    {
+        name: 'song 3',
+        path: 'music/song3.m4a',
+        artist: 'artist 3',
+        cover: 'images/batman.jpg'
+    },
+    {
+        name: 'song 4',
+        path: 'music/song4.mp3',
+        artist: 'artist 4',
+        cover: 'images/gojo.jpg'
+    },
+    {
+        name: 'song 5',
+        path: 'music/song5.mp3',
+        artist: 'artist 5',
+        cover: 'images/HD-wallpaper-junutsu-kaisen-gojo-jujutsu-kaisan.jpg'
+    },
+    {
+        name: 'song 6',
+        path: 'music/song6.mp3',
+        artist: 'artist 6',
+        cover: 'images/wallhaven-49vg98.jpg'
+    }
+]
